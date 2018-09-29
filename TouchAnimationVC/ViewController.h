4c3c7be2caf9616a0bf0a9c2b9c00b495df68549
wangdongshen <dongshen.wang@ftimage.cn>
2018-09-29 13:58:59 +0800

    Initial Commit

---
+++ TouchAnimationVC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TouchAnimationVC
+//
+//  Created by ftimage2 on 2018/9/29.
+//  Copyright © 2018 Jack Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TouchAnimationVC/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  TouchAnimationVC
+//
+//  Created by ftimage2 on 2018/9/29.
+//  Copyright © 2018 Jack Wang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
